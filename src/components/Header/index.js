2f3b4076cf4a3efa5734309abaefcedd0b9e0344
Steven Israel <malenchite@gmail.com>
2021-04-14 11:15:48 -0400

    Updated text in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,8 @@ import "./style.css"
 function Header() {
   return (
     <header className="jumbotron directory-head">
-      <h1 className="text-center">Employee Directory</h1>
+      <h1 className="text-center">Random Inc.</h1>
+      <h3 className="text-center">Employee Directory</h3>
     </header>
   );
 }
