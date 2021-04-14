5cc8fd071e0545b0150dae71c747e3635e1de8b3
Steven Israel <malenchite@gmail.com>
2021-04-14 08:30:52 -0400

    Restricted results to US for ease in formatting

--- src/components/EmployeeList/randomUserAPI.js
+++ src/components/EmployeeList/randomUserAPI.js
@@ -1,6 +1,6 @@
 /* Returns an array of random user data */
 async function randomUsers(count) {
-  const apiURL = "https://randomuser.me/api/?results=" + count;
+  const apiURL = "https://randomuser.me/api/?nat=us&results=" + count;
   const users = await fetch(apiURL)
     .then(response => response.json())
     .then(users => users.results);
